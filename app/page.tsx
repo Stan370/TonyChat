83edf060e7b3d205f67c12dc1a6ed6bf29776ab0
Stan Ke <156306548@qq.com>
2024-02-05 15:48:59 +0800

    carousel

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,22 @@ import Siderbar from './components/Siderbar'
 
 export default function Home() {
   return (
-    <div className="relative h-screen min-h-screen flex overflow-hidden bg-gray-50 dark:bg-[#17171a] dark:text-red-50 py-6 ">
+    <div className="relative h-screen min-h-screen flex overflow-hidden bg-gray-50 dark:bg-[#17171a] dark:text-red-50  ">
       <Siderbar></Siderbar>
+      <div className="section">
+        <div id="default-carousel" className="relative w-full" data-carousel="slide">
+          carousel
+        </div>
+
+      </div>
+      
       <article>
-        <h2>Article Title</h2>
+        <h2>Find & explore customized agents</h2>
         <p>This is the main content of the article.</p>
       </article>
 
       <section>
-        <h2>Another Section</h2>
+        <h2>Another Section</h2> 
         <p>Some more content in another section.</p>
       </section>
 
