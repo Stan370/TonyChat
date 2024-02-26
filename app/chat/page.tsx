fe94fbd547a5f4f152101622049db129cae34168
Stan Ke <156306548@qq.com>
2024-02-26 14:48:30 +0800

    support gemini

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import Siderbar from "../components/Siderbar";
 
 const Chat = () => {
-    const [message, setMessage] = useState('');
-    const [conversations, setConversations] = useState(0);
+  const [message, setMessage] = useState("");
+  const [conversations, setConversations] = useState(0);
+  
   return (
     <div className="relative min-h-screen flex flex-row  bg-gray-50 dark:bg-[#17171a] dark:text-red-50  ">
       <Siderbar></Siderbar>
+      <div className="flex flex-col overflow-y-auto">
+        <div class="flex h-16 w-full flex-shrink-0"> </div>
+      </div>
+
       <div className="p-4 max-w-md mx-auto bg-white shadow-md rounded-lg">
         <div className="mb-4 h-64 overflow-y-auto">
           {conversations.map((text, index) => (
